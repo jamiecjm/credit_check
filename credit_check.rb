b3f80a5fe07d1907f07cf199259fce930c89b3b3
jamiecjm <jamiecjm@gmail.com>
2017-07-12 21:34:15 +1000

    Pass test for all method

--- credit_check.rb
+++ credit_check.rb
@@ -5,8 +5,11 @@ class CreditCheck
 	end
 
 	def valid?
+		# convert string into integer
 		numbers = @card_number.reverse.chars.map(&:to_i)
+		# sum all integer
 		numbers.each_with_index.inject(0) { |sum,(i,j)| 
+			# times 2 to every 2nd number and sum the digits
 			num = j.odd? ? (i*2).divmod(10).inject(:+) : i;
 			sum+num
 		}%10 == 0
---
+++ test.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+require_relative 'credit_check'
+describe "CreditCheck" do
+  let(:correct_data) { "342804633855673" }
+  let(:wrong_data) { "342801633855673" }
+  let(:correct_check) { CreditCheck.new(correct_data) }
+  let(:wrong_check) { CreditCheck.new(wrong_data) }
+
+  describe "initializing a new CreditCheck object" do
+    it "should only take in a string argument" do
+      expect(correct_check).to be_a(CreditCheck)
+    end
+  end
+
+  describe "valid?" do
+    it "should return true if number is valid" do
+      expect(correct_check.valid?).to eq(true)
+    end
+
+    it "should return true if number is valid" do
+      expect(wrong_check.valid?).to eq(false)
+    end
+  end
+
+end
