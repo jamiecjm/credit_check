0d2d0750e6c906e735e66740672cfe2cfcde8020
jamiecjm <jamiecjm@gmail.com>
2017-07-12 22:26:12 +1000

    Use flat_map

--- credit_check.rb
+++ credit_check.rb
@@ -1,16 +1,16 @@
+require 'byebug'
 class CreditCheck
 	def initialize(card_number)
 		@card_number = card_number
 	end
 
 	def valid?
-		# convert string into integer
-		numbers = @card_number.reverse.chars.map(&:to_i)
-		# sum all digits
-		numbers.each_with_index.inject(0) { |sum,(i,j)| 
-			# times 2 to every 2nd number and sum the digits
-			num = j.odd? ? (i*2).divmod(10).inject(:+) : i;
-			sum+num
-		}%10 == 0
+		# reverse card_number and times 2 for every second number
+		numbers = @card_number.reverse.chars.each_slice(2).flat_map {|odd,even| [odd.to_i,(even.to_i*2).divmod(10)]}
+		# remove nested brackets and sum all digits
+		numbers.flatten.inject(:+)%10 == 0
 	end
 end
+
+cc = CreditCheck.new("342804633855673")
+cc.valid?
